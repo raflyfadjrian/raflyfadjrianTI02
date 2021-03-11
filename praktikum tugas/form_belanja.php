b3f3ebb024bced3f2126a6e1516ce8eddc911be0
raflyfadjrian <72740513+raflyfadjrian@users.noreply.github.com>
2021-03-11 23:41:49 +0700

    tugas praktikum

---
+++ praktikum tugas/form_belanja.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<form method="POST" action="form_belanja.php">
+  <div class="form-group row">
+    <label for="customer" class="col-4 col-form-label">Customer</label> 
+    <div class="col-8">
+      <input id="customer" name="customer" placeholder="Nama Customer" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Pilih Produk</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="produk" id="produk_0" type="radio" class="custom-control-input" value="TV"> 
+        <label for="produk_0" class="custom-control-label">TV</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="produk" id="produk_1" type="radio" class="custom-control-input" value="Kulkas"> 
+        <label for="produk_1" class="custom-control-label">KULKAS</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="produk" id="produk_2" type="radio" class="custom-control-input" value="Mesin Cuci"> 
+        <label for="produk_2" class="custom-control-label">MESIN CUCI</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="jumlah" class="col-4 col-form-label">Jumlah Beli</label> 
+    <div class="col-8">
+      <input id="jumlah" name="jumlah" placeholder="Jumlah" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="proses" type="submit" class="btn btn-primary">Kirim</button>
+    </div>
+  </div>
+</form>
+
+<?php
+$proses = $_POST['proses'];
+$nama_customer = $_POST['customer'];
+$produk = $_POST['produk'];
+$jumlah = $_POST['jumlah'];
+
+echo '<br/>Nama Customer : '.$nama_customer;
+echo '<br/>Produk Pilihan : '.$produk;
+echo '<br/>Jumlah Beli : '.$jumlah;
+
+?>
+
+</body>
+</html>
